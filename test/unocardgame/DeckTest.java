019cf5bd22e3e61ca44a80b5211462a020a33e10
sheenaa <sheenaa@sheridancollege.ca>
2019-04-17 23:21:45 -0400

    Base Code for Project UNO is decided game

---
+++ test/unocardgame/DeckTest.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unocardgame;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kd
+ */
+public class DeckTest {
+    
+    public DeckTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of isEmpty method, of class Deck.
+     */
+    @Test
+    public void testIsEmpty() {
+        System.out.println("isEmpty");
+        Deck instance = new Deck();
+        boolean expResult = false;
+        boolean result = instance.isEmpty();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of shuffle method, of class Deck.
+     */
+    @Test
+    public void testShuffle() {
+        System.out.println("shuffle");
+        Deck instance = new Deck();
+        instance.shuffle();
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of draw method, of class Deck.
+     */
+    @Test
+    public void testDraw() {
+        System.out.println("draw");
+        Deck instance = new Deck();
+        Card expResult = null;
+        Card result = instance.draw();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of check method, of class Deck.
+     */
+    @Test
+    public void testCheck() {
+        System.out.println("check");
+        Deck instance = new Deck();
+        Card expResult = null;
+        Card result = instance.check();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
